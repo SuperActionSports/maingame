055c3b7a3b836c78f9ec4b48d42aa399fefcddfe
mp3gans <mp3gans@yahoo.com>
2015-09-05 12:14:51 -0400

    Timebox 1 work
    Super basic level select with main menu, level select, credits

---
+++ LevelSelect (Merge better)/Super Action Sports Menus/Assets/Scripts/levelScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class levelScript : MonoBehaviour {
+	
+	public Button l1;
+	public Button l2;
+	public Button l3;
+	public Button backButton;
+	public string level1name;
+	public string level2name;
+	public string level3name;
+	
+	// Use this for initialization
+	void Start () {
+		l1 = l1.GetComponent<Button> ();
+		l2 = l2.GetComponent<Button> ();
+		l3 = l3.GetComponent<Button> ();
+		backButton = backButton.GetComponent<Button> ();
+	}
+	
+	public void StartLevel1() {
+		Application.LoadLevel (level1name);
+	}
+
+	public void StartLevel2() {
+		Application.LoadLevel (level2name);
+	}
+
+	public void StartLevel3() {
+		Application.LoadLevel (level3name);
+	}
+	
+	public void MainMenu() {
+		Application.LoadLevel ("TitleScreen");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
