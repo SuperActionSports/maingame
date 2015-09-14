63b0ac176e715b3ad104005bb590e9a0fff65319
mathyoumore <mohr.73@osu.edu>
2015-09-14 09:19:59 -0400

    Player Movement Start Branch
    Starting the player movement branch

--- SAS/Assets/Scripts/PlayerController.cs
+++ SAS/Assets/Scripts/PlayerController.cs
@@ -13,19 +13,31 @@ public class PlayerController : MonoBehaviour {
 	public KeyCode right;
 	public KeyCode up;
 	public KeyCode down;
+	
+	public bool alive;
+	
+	private Rigidbody rb;
+	private RaycastHit groundHit;
+	
 	[Range(1,20)]
 	public float speedMagnitude;
 	// Use this for initialization
 	void Start () {
 		rend = GetComponent<Renderer>();
+		rb = GetComponent<Rigidbody>();
 		//rend.material.color = c;
 		speedMagnitude = 10f;
 		complete = false;
 		colorLerpT = 0;
+		alive = true;
+		ResetRigidBodyConstraints();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	if (alive) 
+	{
+			Physics.Raycast(transform.position, Vector3.down,out groundHit, 1.1f);
 	Vector3 speed = new Vector3(0,0,0);
 		if (Input.GetKey(left)) {
 			speed.x -= speedMagnitude;
@@ -33,14 +45,17 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKey(right)) {
 			speed.x += speedMagnitude;
 		}
-		if (Input.GetKey(up)) {
+		if (Input.GetKey(up) && groundHit.collider.CompareTag("Stage")) {
 			speed.y += speedMagnitude;
 		}
 		if (Input.GetKey(down)) {
 			speed.y -= speedMagnitude;
 		}
-		transform.position += speed*Time.deltaTime;
+		//transform.position += speed*Time.deltaTime;
+		rb.AddForce(new Vector3(speed.x, 0,0), ForceMode.Force);
+		rb.AddForce (new Vector3(0,speed.y, 0), ForceMode.VelocityChange);
 		
+		// This can be cut whenever, it changes the colors of the players for easier identification
 		colorLerpT += Time.deltaTime;
 		if (complete) {
 			rend.material.color = Color.Lerp(c2,c1,colorLerpT);
@@ -54,7 +69,29 @@ public class PlayerController : MonoBehaviour {
 			if (colorLerpT >= 1) {
 				complete = true;
 				colorLerpT = 0;
+				}
+			}
+			if (groundHit.collider.CompareTag("Stage"))
+			{
+				//transform.position = new Vector3(transform.position.x, transform.position.y +1, transform.position.z);
 			}
 		}
+		else { //Dead
+			rb.constraints = RigidbodyConstraints.None;
+		}
+		
+		if (Input.GetKey(KeyCode.X))
+		{
+			alive = !alive;
+			if (alive)
+				ResetRigidBodyConstraints();
+		}
 	}
+	
+	private void ResetRigidBodyConstraints() 
+	{
+		rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+		transform.rotation = Quaternion.identity;
+	}
+	
 }
