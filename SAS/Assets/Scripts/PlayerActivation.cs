3d187dd99528c0d9610142fe9555475744c74f75
mp3gans <mp3gans@yahoo.com>
2015-09-21 11:36:18 -0400

    Player Activation and joystick movement (need to continue to fix movement)

--- SAS/Assets/Scripts/PlayerActivation.cs
+++ SAS/Assets/Scripts/PlayerActivation.cs
@@ -33,44 +33,78 @@ public class PlayerActivation : MonoBehaviour {
 			number_of_players = PlayerPrefs.GetInt ("number_of_players");
 		}
 		catch (Exception e) {
-			print("Error: Unable to get the number of players from PlayerPrefs");
+			print ("Error: Unable to get the number of players from PlayerPrefs");
 			number_of_players = 1;
 		}
-
+		/*
 		if(number_of_players>=1){
-			//Retrieving Player 1's Settings and Activating
+			//Retrieving Player 1's Settings
 			player_1 = GameObject.Find ("player_1");
-			player_1.SetActive (true);
 			player_1_color = new int[4];
-			player_1_color = PlayerPrefs.GetInt ("player_1_color");
+			player_1_color[0] = PlayerPrefs.GetInt ("player_1_color_r");
+			player_1_color[1] = PlayerPrefs.GetInt ("player_1_color_g");
+			player_1_color[2] = PlayerPrefs.GetInt ("player_1_color_b");
+			player_1_color[3] = PlayerPrefs.GetInt ("player_1_color_a");
 			player_1_controller_num = PlayerPrefs.GetInt ("player_1_controller_num");
 		}
 
 		if(number_of_players>=2){
-			//Retrieving Player 2's Settings and Activating
+			//Retrieving Player 2's Settings
 			player_2 = GameObject.Find ("player_2");
-			player_2.SetActive (true);
 			player_2_color = new int[4];
-			player_2_color = PlayerPrefs.GetInt ("player_2_color");
+			player_2_color[0] = PlayerPrefs.GetInt ("player_2_color_r");
+			player_2_color[1] = PlayerPrefs.GetInt ("player_2_color_g");
+			player_2_color[2] = PlayerPrefs.GetInt ("player_2_color_b");
+			player_2_color[3] = PlayerPrefs.GetInt ("player_2_color_a");
 			player_2_controller_num = PlayerPrefs.GetInt ("player_2_controller_num");
 		}
 
 		if(number_of_players>=3){
-			//Retrieving Player 3's Settings and Activating
+			//Retrieving Player 3's Settings
 			player_3 = GameObject.Find ("player_3");
-			player_3.SetActive (true);
 			player_3_color = new int[4];
-			player_3_color = PlayerPrefs.GetInt ("player_3_color");
+			player_3_color[0] = PlayerPrefs.GetInt ("player_3_color_r");
+			player_3_color[1] = PlayerPrefs.GetInt ("player_3_color_g");
+			player_3_color[2] = PlayerPrefs.GetInt ("player_3_color_b");
+			player_3_color[3] = PlayerPrefs.GetInt ("player_3_color_a");
 			player_3_controller_num = PlayerPrefs.GetInt ("player_3_controller_num");
 		}
 
 		if(number_of_players>=4){
-			//Retrieving Player 4's Settings and Activating
+			//Retrieving Player 4's Settings
 			player_4 = GameObject.Find ("player_4");
-			player_4.SetActive (true);
 			player_4_color = new int[4];
-			player_4_color = PlayerPrefs.GetInt ("player_4_color");
+			player_4_color[0] = PlayerPrefs.GetInt ("player_4_color_r");
+			player_4_color[1] = PlayerPrefs.GetInt ("player_4_color_g");
+			player_4_color[2] = PlayerPrefs.GetInt ("player_4_color_b");
+			player_4_color[3] = PlayerPrefs.GetInt ("player_4_color_a");
 			player_4_controller_num = PlayerPrefs.GetInt ("player_4_controller_num");
+		}
+*/
+		if (number_of_players == 2) {
+			player_1.SetActive (true);
+			player_2.SetActive (true);
+			player_3.SetActive (false);
+			player_4.SetActive (false);
+		}
+		else if (number_of_players == 3) {
+			player_1.SetActive (true);
+			player_2.SetActive (true);
+			player_3.SetActive (true);
+			player_4.SetActive (false);
+		}
+		else if (number_of_players == 4) {
+			player_1.SetActive (true);
+			player_2.SetActive (true);
+			player_3.SetActive (true);
+			player_4.SetActive (true);
+		}
+		else {
+			player_1.SetActive (true);
+			player_2.SetActive (true);
+			player_3.SetActive (false);
+			player_4.SetActive (false);
+			number_of_players = 2;
 		}
 
 	}
@@ -83,40 +117,66 @@ public class PlayerActivation : MonoBehaviour {
 		//--------------------FOR TESTING PURPOSES ONLY - TO ALLOW MID-GAME CHANGES (BELOW)--------------------//
 		//-----------------------------------------------------------------------------------------------------//
 		//-----------------------------------------------------------------------------------------------------//
-		if(number_of_players>=1){
-			//Retrieving Player 1's Settings and Activating
-			player_1 = GameObject.Find ("player_1");
-			player_1.SetActive (true);
-			player_1_color = new int[4];
-			player_1_color = PlayerPrefs.GetInt ("player_1_color");
+		/*if(number_of_players>=1){
+			//Retrieving Player 1's Settings
+			player_1_color[0] = PlayerPrefs.GetInt ("player_1_color_r");
+			player_1_color[1] = PlayerPrefs.GetInt ("player_1_color_g");
+			player_1_color[2] = PlayerPrefs.GetInt ("player_1_color_b");
+			player_1_color[3] = PlayerPrefs.GetInt ("player_1_color_a");
 			player_1_controller_num = PlayerPrefs.GetInt ("player_1_controller_num");
 		}
 		
 		if(number_of_players>=2){
-			//Retrieving Player 2's Settings and Activating
-			player_2 = GameObject.Find ("player_2");
-			player_2.SetActive (true);
-			player_2_color = new int[4];
-			player_2_color = PlayerPrefs.GetInt ("player_2_color");
+			//Retrieving Player 2's Settings
+			player_2_color[0] = PlayerPrefs.GetInt ("player_2_color_r");
+			player_2_color[1] = PlayerPrefs.GetInt ("player_2_color_g");
+			player_2_color[2] = PlayerPrefs.GetInt ("player_2_color_b");
+			player_2_color[3] = PlayerPrefs.GetInt ("player_2_color_a");
 			player_2_controller_num = PlayerPrefs.GetInt ("player_2_controller_num");
 		}
 		
 		if(number_of_players>=3){
-			//Retrieving Player 3's Settings and Activating
-			player_3 = GameObject.Find ("player_3");
-			player_3.SetActive (true);
-			player_3_color = new int[4];
-			player_3_color = PlayerPrefs.GetInt ("player_3_color");
+			//Retrieving Player 3's Settings
+			player_3_color[0] = PlayerPrefs.GetInt ("player_3_color_r");
+			player_3_color[1] = PlayerPrefs.GetInt ("player_3_color_g");
+			player_3_color[2] = PlayerPrefs.GetInt ("player_3_color_b");
+			player_3_color[3] = PlayerPrefs.GetInt ("player_3_color_a");
 			player_3_controller_num = PlayerPrefs.GetInt ("player_3_controller_num");
 		}
 		
 		if(number_of_players>=4){
-			//Retrieving Player 4's Settings and Activating
-			player_4 = GameObject.Find ("player_4");
-			player_4.SetActive (true);
-			player_4_color = new int[4];
-			player_4_color = PlayerPrefs.GetInt ("player_4_color");
+			//Retrieving Player 4's Settings
+			player_4_color[0] = PlayerPrefs.GetInt ("player_4_color_r");
+			player_4_color[1] = PlayerPrefs.GetInt ("player_4_color_g");
+			player_4_color[2] = PlayerPrefs.GetInt ("player_4_color_b");
+			player_4_color[3] = PlayerPrefs.GetInt ("player_4_color_a");
 			player_4_controller_num = PlayerPrefs.GetInt ("player_4_controller_num");
+		}*/
+		
+		if (number_of_players == 2) {
+			player_1.SetActive (true);
+			player_2.SetActive (true);
+			player_3.SetActive (false);
+			player_4.SetActive (false);
+		}
+		else if (number_of_players == 3) {
+			player_1.SetActive (true);
+			player_2.SetActive (true);
+			player_3.SetActive (true);
+			player_4.SetActive (false);
+		}
+		else if (number_of_players == 4) {
+			player_1.SetActive (true);
+			player_2.SetActive (true);
+			player_3.SetActive (true);
+			player_4.SetActive (true);
+		}
+		else {
+			player_1.SetActive (true);
+			player_2.SetActive (false);
+			player_3.SetActive (false);
+			player_4.SetActive (false);
+			number_of_players = 1;
 		}
 		//-----------------------------------------------------------------------------------------------------//
 		//-----------------------------------------------------------------------------------------------------//
---
+++ SAS/Assets/Scripts/PlayerMovementControllerCombination.cs
@@ -0,0 +1,232 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementControllerCombination : MonoBehaviour {
+
+	private Vector3 movementVector;
+	private CharacterController characterController;
+	private float movementSpeed = 8;
+	private float jumpPower = 15;
+	private float gravity = 40;
+	public int joystickNumber;
+
+	public Color c1;
+	private bool colorChangeToUniform;
+	private Renderer rend;
+	private float colorLerpT;
+	private bool facingLeft;
+	
+	public KeyCode left;
+	public KeyCode right;
+	public KeyCode up;
+	public KeyCode down;
+	public KeyCode attack;
+	public KeyCode debugKill;
+	
+	public bool alive;
+	
+	private Rigidbody rb;
+	public RaycastHit groundHit;
+	public float magSpeedX;
+	public float magSpeedY;
+	public float debugYMod;
+	public Vector3 speed;
+	private bool doubleJumpAllowed;
+	
+	public GameObject[] respawnPoints;
+	public GameObject equipment;
+	private CapsuleCollider equipmentCollider;
+	
+	private Animator anim;
+	
+	[Range(1,20)]
+	public float speedMagnitude;
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+		rend = GetComponent<Renderer>();
+		rb = GetComponent<Rigidbody>();
+		anim = GetComponent <Animator>();
+		equipmentCollider = equipment.GetComponent<CapsuleCollider>();
+		//rend.material.color = c;
+		speedMagnitude = 10f;
+		colorChangeToUniform = false;
+		colorLerpT = 0;
+		alive = true;
+		anim.SetBool("Alive", true);
+		ResetRigidBodyConstraints();
+		doubleJumpAllowed = true;
+		
+		respawnPoints = GameObject.FindGameObjectsWithTag("RespawnPoint");
+		
+		if (respawnPoints.Length == 0)
+		{
+			Debug.Log("There aren't any respawn points, you catastrophic dingus.");
+		}
+		
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		string joystickString = joystickNumber.ToString();
+		if (alive) 
+		{
+			magSpeedX = 0;
+			magSpeedY = 0;
+			movementVector.x = Input.GetAxis("LeftJoystickX_P" + joystickString) * movementSpeed;
+			movementVector.z = -1*Input.GetAxis("LeftJoystickY_P" + joystickString) * movementSpeed;
+			if (Input.GetKey(left)) {
+				magSpeedX = -1;
+			}
+			if (Input.GetKey(right)) {
+				magSpeedX = 1;
+			}
+			if (Input.GetButtonDown("A_P" + joystickString))
+			{
+				if (doubleJumpAllowed)
+				{
+					rb.velocity = new Vector3(rb.velocity.x,speedMagnitude*4f,rb.velocity.z);
+					doubleJumpAllowed = false;
+				}
+				else if (Physics.Raycast(transform.position, Vector3.down, out groundHit, 1.1f))
+				{
+					if (groundHit.collider.CompareTag("Stage")) {
+						doubleJumpAllowed = true;
+						rb.velocity = new Vector3(rb.velocity.x, speedMagnitude*4f, rb.velocity.z);
+					}
+				}
+			}
+			if (Input.GetKey(down)) {
+				rb.velocity -= new Vector3 (0, speedMagnitude * Time.deltaTime, 0);
+			}
+			
+			float xSpeed = speedMagnitude * magSpeedX;
+			speed = new Vector3(xSpeed, 0, 0);
+			transform.position = transform.position + new Vector3(speed.x * Time.deltaTime, 0, 0);
+			if (speed.x < 0)
+			{
+				transform.rotation = new Quaternion(transform.rotation.x, 180f, transform.rotation.z, transform.rotation.w);
+			}
+			else if (speed.x > 0 && transform.rotation.y > 0)
+			{
+				transform.rotation =  new Quaternion(transform.rotation.x, 0f, transform.rotation.z, transform.rotation.w);
+			}
+		}	
+		
+		UpdateColor();
+		
+		if (Input.GetKeyDown(debugKill))
+		{
+			alive = !alive;
+			if (alive)
+			{
+				Respawn();
+			}
+		}
+		
+		if (Input.GetButton("X_P" + joystickString))
+		{
+			anim.SetTrigger("Attack");
+		}
+		if (anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+		{
+			equipmentCollider.enabled = true;
+		}
+		else if (equipmentCollider.enabled)
+		{
+			equipmentCollider.enabled = false;
+		}
+		if(characterController.isGrounded)
+		{
+			movementVector.y = 0;
+			
+			if(Input.GetButtonDown("A_P" + joystickString))
+			{
+				movementVector.y = jumpPower; 
+			}
+		}
+		movementVector.y -= gravity * Time.deltaTime;
+		characterController.Move(movementVector * Time.deltaTime);
+	}
+	
+	private void ResetRigidBodyConstraints() 
+	{
+		rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+		transform.rotation = Quaternion.identity;
+	}
+	
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - 1.1f, transform.position.z));
+	}
+	
+	public void Kill()
+	{
+		rb.AddForce(40, 25, 0, ForceMode.VelocityChange);
+		rb.constraints = RigidbodyConstraints.None;
+		alive = false;
+		anim.SetBool("Alive", false);
+		/* foreach (Transform child in transform)
+        {
+            Vector3 t = child.transform.TransformPoint(child.transform.position);
+            child.parent = null;
+            child.transform.position = t;
+
+        }*/
+	}
+	
+	public void Kill(Vector3 direction)
+	{
+		
+		rb.AddForce(Vector3.Cross(new Vector3(4, 4, 0), direction), ForceMode.VelocityChange);
+		rb.constraints = RigidbodyConstraints.None;
+		alive = false;
+		anim.SetBool("Alive", false);
+		/* foreach (Transform child in transform)
+         {
+             Vector3 t = child.transform.TransformPoint(child.transform.position);
+             child.parent = null;
+             child.transform.position = t;
+
+         }*/
+	}
+	
+	public void Respawn()
+	{
+		ResetRigidBodyConstraints();
+		rb.velocity = new Vector3(0, 0, 0);
+		anim.SetBool("Alive", true);
+		Debug.Log("Length: " + respawnPoints.Length);
+		transform.position = respawnPoints[Mathf.FloorToInt(Random.Range(0, respawnPoints.Length))].transform.position;
+		colorChangeToUniform = true;
+	}
+	
+	private void UpdateColor()
+	{
+		colorLerpT += Time.deltaTime;
+		if (colorChangeToUniform && alive)
+		{
+			rend.material.color = Color.Lerp(new Color(0, 0, 0, 0), c1, colorLerpT);
+			if (colorLerpT >= 1)
+			{
+				colorChangeToUniform = false;
+				colorLerpT = 0;
+			}
+		}
+		else
+		{
+			rend.material.color = Color.Lerp(c1, new Color(0, 0, 0, 0), colorLerpT);
+			if (colorLerpT >= 1)
+			{
+				if (alive)
+				{
+					colorChangeToUniform = true;
+					colorLerpT = 0;
+				}
+			}
+		}
+	}
+}
