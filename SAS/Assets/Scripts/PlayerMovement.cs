f6bb765557e0066f7b24611a034976cba29f78c6
mp3gans <mp3gans@yahoo.com>
2015-09-21 09:15:59 -0400

    player color selection update

---
+++ SAS/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	
+	private Vector3 movementVector;
+	private CharacterController characterController;
+	private float movementSpeed = 8;
+	private float jumpPower = 15;
+	private float gravity = 40;
+	public int joystickNumber;
+	
+	
+	void Start()
+	{
+		characterController = GetComponent<CharacterController>();
+	}
+	
+	
+	void Update()
+	{
+		string joystickString = joystickNumber.ToString();
+		
+		movementVector.x = Input.GetAxis("LeftJoystickX_P" + joystickString) * movementSpeed;
+		movementVector.z = -1*Input.GetAxis("LeftJoystickY_P" + joystickString) * movementSpeed;
+		
+		if(characterController.isGrounded)
+		{
+			movementVector.y = 0;
+			
+			if(Input.GetButtonDown("A_P" + joystickString))
+			{
+				movementVector.y = jumpPower; 
+			}
+		}
+		
+		movementVector.y -= gravity * Time.deltaTime;
+		characterController.Move(movementVector * Time.deltaTime);
+	}
+}
