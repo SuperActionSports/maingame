26e509c94ae5cb843d6b7117f5862e1e9552f97e
mathyoumore <mohr.73@osu.edu>
2015-09-23 23:24:38 -0400

    Gitmerge for Unity Initial push
    Example scene to mess up in the remote and local to test the capabilites
    of GitMerge for Unity

---
+++ GitMerge for Unity Test/Assets/Slidey.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slidey : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.position = new Vector3 (4*Mathf.Sin(Time.time*4), 5,-7);
+	}
+}
