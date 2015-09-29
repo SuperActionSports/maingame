ae853db6942565e2866c27326c08b36eea4b1dd1
mp3gans <mp3gans@yahoo.com>
2015-09-29 19:37:11 -0400

    Color selection working and saving to playerprefs

--- SAS/Assets/Scripts/Menu/AdvancedPlayerColorHueSelection.cs
+++ SAS/Assets/Scripts/Menu/AdvancedPlayerColorHueSelection.cs
@@ -12,6 +12,7 @@ public class AdvancedPlayerColorHueSelection : MonoBehaviour {
 	public float sliderVal;
 	public int times;
 	public float remainder;
+	public int player_num;
 	// Use this for initialization
 	void Start () {
 		player_slider.maxValue = 1785;
@@ -24,13 +25,12 @@ public class AdvancedPlayerColorHueSelection : MonoBehaviour {
 		player_Color (player_slider.value);
 		splat.color = new Color (playerR, playerG, playerB);
 		sliderVal = (int)player_slider.value;
+	
 	}
 
 	void player_Color(float slideNum){
 		times = (int)(slideNum / 255f);
 		remainder = slideNum % 255;
-
-
 
 		switch (times) {
 		case 0:
@@ -74,12 +74,31 @@ public class AdvancedPlayerColorHueSelection : MonoBehaviour {
 		case 7:
 			playerR = playerG = playerB = 1;
 			break;
-		/*default:
-			playerR = 255;
-			playerG = 0;
-			playerB = 0;
-			break;*/
+		}
+	}
 
+	public void SavePlayerPref() {
+		switch(player_num){
+		case 1:
+			PlayerPrefs.SetFloat("player_1_colorR",playerR);
+			PlayerPrefs.SetFloat("player_1_colorG",playerG);
+			PlayerPrefs.SetFloat("player_1_colorB",playerB);
+			break;
+		case 2:
+			PlayerPrefs.SetFloat("player_2_colorR",playerR);
+			PlayerPrefs.SetFloat("player_2_colorG",playerG);
+			PlayerPrefs.SetFloat("player_2_colorB",playerB);
+			break;
+		case 3:
+			PlayerPrefs.SetFloat("player_3_colorR",playerR);
+			PlayerPrefs.SetFloat("player_3_colorG",playerG);
+			PlayerPrefs.SetFloat("player_3_colorB",playerB);
+			break;
+		case 4:
+			PlayerPrefs.SetFloat("player_4_colorR",playerR);
+			PlayerPrefs.SetFloat("player_4_colorG",playerG);
+			PlayerPrefs.SetFloat("player_4_colorB",playerB);
+			break;
 		}
 	}
 
---
+++ SAS/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInfo : MonoBehaviour {
+
+	public int playerNumber;
+	public int numOfPlayers;
+	public float[] player_1_color;
+	public float[] player_2_color;
+	public float[] player_3_color;
+	public float[] player_4_color;
+	public float playerR;
+	public float playerG;
+	public float playerB;
+	
+	// Use this for initialization
+	void Start () {
+		playerNumber = 0;
+		numOfPlayers = 0;
+		player_1_color = new float[3];
+		player_2_color = new float[3];
+		player_3_color = new float[3];
+		player_4_color = new float[3];
+
+		numOfPlayers = PlayerPrefs.GetInt("numOfPlayers");
+
+		player_1_color[0] = PlayerPrefs.GetFloat ("player_1_colorR");
+		player_1_color[1] = PlayerPrefs.GetFloat ("player_1_colorG");
+		player_1_color[2] = PlayerPrefs.GetFloat ("player_1_colorB");
+
+		player_2_color[0] = PlayerPrefs.GetFloat ("player_2_colorR");
+		player_2_color[1] = PlayerPrefs.GetFloat ("player_2_colorG");
+		player_2_color[2] = PlayerPrefs.GetFloat ("player_2_colorB");
+
+		player_3_color[0] = PlayerPrefs.GetFloat ("player_3_colorR");
+		player_3_color[1] = PlayerPrefs.GetFloat ("player_3_colorG");
+		player_3_color[2] = PlayerPrefs.GetFloat ("player_3_colorB");
+
+		player_4_color[0] = PlayerPrefs.GetFloat ("player_4_colorR");
+		player_4_color[1] = PlayerPrefs.GetFloat ("player_4_colorG");
+		player_4_color[2] = PlayerPrefs.GetFloat ("player_4_colorB");
+
+		playerR = PlayerPrefs.GetFloat ("player_1_colorR");
+		playerG = PlayerPrefs.GetFloat ("player_1_colorG");
+		playerB = PlayerPrefs.GetFloat ("player_1_colorB");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
