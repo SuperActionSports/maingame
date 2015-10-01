556537a59613a02da7a76cae42a7ecf413f1566c
AUSTIN <grosel.9@osu.edu>
2015-10-01 18:15:35 -0400

    First WIP
    Yep that’s it

---
+++ SAS/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+	Vector3 position;
+	Rigidbody rb;
+	Collider collider;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+		rb = GetComponent<Rigidbody> ();
+		rb.useGravity = true;
+		rb.AddForce (transform.forward * 5000);
+		collider = GetComponent<SphereCollider> ();
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Player") {
+			Physics.IgnoreCollision(collision.collider, collider);
+			Debug.Log ("Hit player :(");
+		}
+		if (collision.gameObject.tag == "BackWall") {
+			rb.AddForce(transform.forward * -1000);
+			Debug.Log ("Hit back wall");
+		}
+		if (collision.gameObject.tag == "FrontWall") {
+			rb.AddForce(transform.forward * 1000);
+			Debug.Log ("Hit front wall");
+		}
+	}
+}
---
+++ SAS/Assets/Scripts/TennisAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TennisAction : MonoBehaviour {
+
+	public GameObject tennisBall;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			tennisBall.SetActive(true);
+		}
+
+	}
+}
