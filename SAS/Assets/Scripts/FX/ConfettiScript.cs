3eaafae32d3bfdbba0019535ecbc2dd8ee1bdb2c
mathyoumore <mohr.73@osu.edu>
2015-10-01 18:03:36 -0400

    Checkout, some small adjustments to confetti

--- SAS/Assets/Scripts/FX/ConfettiScript.cs
+++ SAS/Assets/Scripts/FX/ConfettiScript.cs
@@ -6,10 +6,11 @@ public class ConfettiScript : MonoBehaviour {
 	public KeyCode replay;
 	public Color c;
 	public ParticleSystem[] parts;
+	public ParticleAnimator[] partAnimators;
 	// Use this for initialization
 	void Start () {
 		parts = GetComponentsInChildren<ParticleSystem>();
-		
+		partAnimators = GetComponentsInChildren<ParticleAnimator>();
 		Debug.Log(parts[0]);
 	}
 	
@@ -20,6 +21,7 @@ public class ConfettiScript : MonoBehaviour {
 		 	foreach (ParticleSystem p in parts)
 		 	{
 		 		p.Play ();
+		 		
 		 	}
 		 }
 	}
