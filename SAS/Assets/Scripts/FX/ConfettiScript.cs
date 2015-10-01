3802ad0570633c1ee4dedc443c08df31ea62526c
mathyoumore <mohr.73@osu.edu>
2015-10-01 16:03:56 -0400

    Confetti PS
    Initial stab at a confetti PS for deaths, points, whatever.

---
+++ SAS/Assets/Scripts/FX/ConfettiScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfettiScript : MonoBehaviour {
+
+	public KeyCode replay;
+	public Color c;
+	public ParticleSystem[] parts;
+	// Use this for initialization
+	void Start () {
+		parts = GetComponentsInChildren<ParticleSystem>();
+		
+		Debug.Log(parts[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if (Input.GetKeyDown(replay))
+		 {
+		 	foreach (ParticleSystem p in parts)
+		 	{
+		 		p.Play ();
+		 	}
+		 }
+	}
+}
