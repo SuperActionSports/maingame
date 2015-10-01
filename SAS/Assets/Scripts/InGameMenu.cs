edb925c50efd8bfdebbc398b31c12b80e451f714
mp3gans <mp3gans@yahoo.com>
2015-10-01 16:53:28 -0400

    more work on hockey
    also added InGameMenu prefab

---
+++ SAS/Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameMenu : MonoBehaviour {
+
+	public KeyCode escape;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(escape))
+		{
+			Application.LoadLevel ("AdvancedMainMenu");
+		}
+	}
+}
