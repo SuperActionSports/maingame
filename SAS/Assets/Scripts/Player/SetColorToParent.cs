0f1b7ddab7784cf181fe049ab28bfbd4f5f48723
mathyoumore <mohr.73@osu.edu>
2015-10-02 14:13:32 -0400

    Trails finally match the color of owner!
    YEAH!

--- SAS/Assets/Scripts/Player/SetColorToParent.cs
+++ SAS/Assets/Scripts/Player/SetColorToParent.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class SetColorToParent : MonoBehaviour {
 	
 	private float t;
+	public Color trc;
 	// Use this for initialization
 	void Start () {
 	ResetColor();
@@ -25,16 +26,26 @@ public class SetColorToParent : MonoBehaviour {
 		{
 			
 			Renderer rp = transform.parent.GetComponent<Renderer>();
-			r.material.color = Color.Lerp(Color.black,rp.material.color,t);
+			//r.material.color = Color.Lerp(Color.black,rp.material.color,t);
+			r.material.color = rp.material.color;
 		}
 		else{
-			r.material.color = Color.Lerp(r.material.color,Color.black,t);
+			//r.material.color = Color.Lerp(r.material.color,Color.black,t);
+			r.material.color = Color.black;
+		}
+		TrailRenderer tr = GetComponent<TrailRenderer>();
+		if (tr != null) 
+		{
+			Debug.Log("Trail is now " + r.material.color); 
+			tr.material.color = r.material.color;
+			trc = tr.material.color;
 		}
 	}
 	
 	public void ResetColor(Color c)
 	{
 		Renderer r = GetComponent<Renderer>();
-		r.material.color = Color.Lerp(Color.black,c,t);
+		//r.material.color = Color.Lerp(Color.black,c,t);
+		r.material.color = c;
 	}
 }
