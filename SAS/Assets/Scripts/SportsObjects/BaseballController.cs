640ec5b16603d420d2c32b541935933005fce19c
Krunchylex <cueto.3@osu.edu>
2015-10-03 13:54:39 -0400

    Attempted rebranch
    Unity is being uncooperative as Hell so let’s see if this works out
    better than the last time I branched.

---
+++ SAS/Assets/Scripts/SportsObjects/BaseballController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class BaseballController : MonoBehaviour {
+
+	public int playerHit = 0 ;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y < 0) {
+			Destroy (gameObject);
+		}
+
+	}
+
+	void OnCollisionEnter (Collision other)
+	{
+		if (other.gameObject.tag == "field") {
+			Destroy (gameObject);
+		} 
+		else if (other.gameObject.tag == "killzone") {
+			Destroy (gameObject);
+			// explosion visual
+			AudioSource audio = GetComponent<AudioSource>() ;
+			audio.Play ();
+		} 
+		else if (other.gameObject.tag == "Equipment") {
+			// set playerHit to player# of player who hit the ball
+		} 
+	}
+}	
---
+++ SAS/Assets/Scripts/SportsObjects/BaseballLauncher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class BaseballLauncher : MonoBehaviour {
+
+	public Rigidbody ball ;
+	private bool loading ;
+
+
+	void Start () {
+		loading = true ;
+		// play 'charge' here
+		Invoke ("Pitch", 4.0f) ;
+	}
+
+	void Update () {
+		if (!loading) {
+			loading = true ;
+			Invoke ("Pitch", 4.0f) ;
+		}
+	}
+				
+	void Pitch () {
+		Rigidbody egg = Instantiate(ball, transform.position, Quaternion.identity) as Rigidbody ;
+		Vector3 pitchPow = new Vector3 (Random.Range(-23, -2), 40, 0) ;
+		egg.AddForce(pitchPow);
+		AudioSource audio = GetComponent<AudioSource>() ;
+		audio.Play ();
+		loading = false ;
+	}
+}
