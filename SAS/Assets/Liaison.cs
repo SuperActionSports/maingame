236e5a8d06ef4d7d672ca26fedf31dc82cc77b44
mp3gans <mp3gans@yahoo.com>
2015-10-10 19:09:32 -0400

    started the liaison script

---
+++ SAS/Assets/Liaison.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Liaison : MonoBehaviour {
+
+	//This script will be the liaison between the Game menu, Incontrol and the individual scenes
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
