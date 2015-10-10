3358279f4b8efd9847b677d4c11308936ad68e0e
tylorlilley <lilley.22@buckeyemail.osu.edu>
2015-10-10 18:00:31 -0400

    Began Work on Hockey
    edited a lot of the player and puck code to clean up and fix player
    movement, turning, and puck collisions.

--- SAS/Assets/Scripts/Hockey/PuckMovement.cs
+++ SAS/Assets/Scripts/Hockey/PuckMovement.cs
@@ -8,28 +8,15 @@ public class PuckMovement : MonoBehaviour {
 	public Color c1;
 	private CapsuleCollider puckCollider;
 	private Rigidbody rb;
-	public bool inplay;
+	public bool inPlay;
 	public GameObject respawnPoint;
 	public InputDevice device {get; set;}
-	//public float momentumX;
-	//public float momentumZ;
-	//[Range(1,200)]
-	//public float maxSpeed;
-	//[Range(0.7f,1.0f)]
-	//public float friction;
-	//public float floatAbove;
-	//[Range(1,20)]
-	//public float speedMagnitude;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody>();
-		//friction = .97f;
-		//maxSpeed = 20;
-		//speedMagnitude = 15;
-		inplay = true;
+		inPlay = true;
 		respawnPoint = GameObject.Find("Puck RespawnPoint");
-
+		rb = GetComponent<Rigidbody> ();
 		if (respawnPoint == null)
 		{
 			Debug.Log("There aren't any respawn points, you catastrophic dingus.");
@@ -38,29 +25,16 @@ public class PuckMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		GetRespawn();
-		if (transform.position.y > 2.26) {
-			rb.constraints = RigidbodyConstraints.None;
-			rb.useGravity = true;
-		} else {
-			rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-			rb.useGravity = false;
-		}
+		//GetRespawn();
 	}
 
 	void OnTriggerEnter (Collider col)
 	{
-		if (inplay == true) {
+		if (inPlay == true) {
 			if (col.gameObject.name == "Inner West Net" || col.gameObject.name == "Inner East Net") {
 				Debug.Log ("Goal Scored");
-				inplay = false;
+				inPlay = false;
 				Respawn();
-			}
-			if (col.gameObject.tag == "Player")
-			{
-				Debug.Log ("player hit puck");
-				Rigidbody playerRB = col.gameObject.GetComponent<Rigidbody>();
-				rb.AddForce (playerRB.velocity);
 			}
 		}
 	}
@@ -69,29 +43,20 @@ public class PuckMovement : MonoBehaviour {
 	{
 		if (Input.GetKeyDown(debugKill) || (device!= null && device.Command.WasPressed))
 		{
-			if (inplay)
-				MakeDead();
-			else
 				Respawn();
 		} 
 	}
 
-	private void MakeDead()
-	{
-		inplay = false;
-		rb.constraints = RigidbodyConstraints.None;
-	}
-
 	public void Respawn()
 	{
-		inplay = true;
+		inPlay = true;
 		rb.velocity = new Vector3(0, 0, 0);
 		transform.position = respawnPoint.transform.position;
 	}
 
 	private void ResetRigidBodyConstraints() 
 	{
-		rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+		rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
 		transform.rotation = Quaternion.identity;
 	}
 }
