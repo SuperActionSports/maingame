7b95db6cb7ed7e3e509f85c03c1578f70e421c67
AUSTIN <grosel.9@osu.edu>
2015-10-13 11:56:18 -0400

    First Commit
    Controller is compatible with menu except player COLOR selection

---
+++ SAS/Assets/Scripts/Menu/Credits/CreditsButtonManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using InControl;
+
+public class CreditsButtonManager : MonoBehaviour
+{
+	public CreditsButton focusedButton;
+
+	TwoAxisInputControl filteredDirection;
+			
+	void Awake()
+	{
+		filteredDirection = new TwoAxisInputControl();
+		filteredDirection.StateThreshold = 0.5f;
+	}
+
+	void Update()
+	{
+		// Use last device which provided input.
+		var inputDevice = InputManager.ActiveDevice;
+		filteredDirection.Filter( inputDevice.Direction, Time.deltaTime );
+		// filteredDirection = inputDevice.Direction;
+
+		if (filteredDirection.Left.WasRepeated)
+		{
+			Debug.Log( "!!!" );
+		}
+			
+		if (inputDevice.Action1.WasPressed || inputDevice.Action2.WasPressed) 
+		{
+			LoadScene();
+		}
+	}
+
+	void LoadScene()
+	{
+		Debug.Log ("Btn: " + focusedButton.name);
+		if (focusedButton.name == "Back") 
+		{
+			Application.LoadLevel ("AdvancedMainMenuControl");
+		}
+	}
+		
+		
+	void MoveFocusTo( CreditsButton newFocusedButton )
+	{
+		if (newFocusedButton != null)
+		{
+			focusedButton = newFocusedButton;
+		}
+	}
+}
---
+++ SAS/Assets/Scripts/Menu/MainMenu/MainMenuButton.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using InControl;
+
+	public class MainMenuButton : MonoBehaviour
+	{
+		Renderer cachedRenderer;
+
+		public MainMenuButton up = null;
+		public MainMenuButton down = null;
+		public MainMenuButton left = null;
+		public MainMenuButton right = null;
+
+
+		void Start()
+		{
+			cachedRenderer = GetComponent<Renderer>();
+		}
+
+
+		void Update()
+		{
+			// Find out if we're the focused button.
+			bool hasFocus = transform.parent.GetComponent<MainMenuButtonManager>().focusedButton == this;
+
+			// Fade alpha in and out depending on focus.
+			//var color = cachedRenderer.material.color;
+			//color.a = Mathf.MoveTowards( color.a, hasFocus ? 1.0f : 0.5f, Time.deltaTime * 3.0f );
+			//cachedRenderer.material.color = color;
+		}
+	}
+
+
