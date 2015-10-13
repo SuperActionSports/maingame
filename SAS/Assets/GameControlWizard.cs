c7c9abc844e8dd23e55de45ca94580abdcaade2f
mp3gans <mp3gans@yahoo.com>
2015-10-13 11:18:54 -0400

    Wizard Progress

--- SAS/Assets/GameControlWizard.cs
+++ SAS/Assets/GameControlWizard.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine;
 using System.IO;
+using InControl;
 using System;
 
 public class GameControlWizard : MonoBehaviour {
@@ -12,6 +13,35 @@ public class GameControlWizard : MonoBehaviour {
 	public float health;
 	public float exp;
 
+	public int number_of_players;
+	public GameObject Player_Prefab;
+
+	public float[] player_1_color;
+	public float[] player_2_color;
+	public float[] player_3_color;
+	public float[] player_4_color;
+
+	public InputDevice player_1_controller;
+	public InputDevice player_2_controller;
+	public InputDevice player_3_controller;
+	public InputDevice player_4_controller;
+
+	public Vector3[] player_1_respawnpoints;
+	public Vector3[] player_2_respawnpoints;
+	public Vector3[] player_3_respawnpoints;
+	public Vector3[] player_4_respawnpoints;
+
+	public string fensing_filename;
+	public string baseball_filename;
+	public string golf_filename;
+	public string tennis_filename;
+	public string hockey_filename;
+
+	public GameObject[] home_team;
+	public GameObject[] away_team;
+	public Vector3[] home_team_respawn_points;
+	public Vector3[] away_team_respawn_points;
+
 	// Use this for code that will execute before Start ()
 	void Awake () { 
 		if (control == null) {
@@ -21,16 +51,80 @@ public class GameControlWizard : MonoBehaviour {
 			Destroy(gameObject);
 		} 
 	}
-
-	// Use this for initialization
-	void Start () {
+	
+	public void LoadFencing() {
+		//Load Level
+		Application.LoadLevel (fensing_filename);
+		//Load Respawn Points
+		GameObject.Find (""); //Insert Respond Points Text
 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void LoadBaseball() {
+		//Load Level
+		Application.LoadLevel (baseball_filename);
+		//Load Respawn Points
+		GameObject.Find (""); //Insert Respond Points Text
+
 	}
+
+	public void LoadGolf() {
+		//Load Level
+		Application.LoadLevel (golf_filename);
+		//Load Respawn Points
+		GameObject.Find (""); //Insert Respond Points Text
+
+	}
+
+	public void LoadTennis() {
+		//Load Level
+		Application.LoadLevel (tennis_filename);
+		//Load Respawn Points
+		GameObject.Find (""); //Insert Respond Points Text
+
+	}
+
+	public void LoadHockey() {
+		//Load Level
+		Application.LoadLevel (hockey_filename);
+		//Load Respawn Points
+		GameObject.Find (""); //Insert Respond Points Text
+
+	}
+
+	public void SetPlayerColor(int player_number) {
+
+	}
+
+	public void SetAllPlayerColor() {
+		
+	}
+
+	public float[] GetPlayerColor(int player_number) {
+		return null;
+	}
+
+	public float[] GetAllPlayerColor() {
+		return null;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	public void Save() {
 		BinaryFormatter bf = new BinaryFormatter ();
@@ -61,4 +155,12 @@ public class GameControlWizard : MonoBehaviour {
 class GameData {
 	public float health;
 	public float exp;
+}
+
+[Serializable]
+class Player {
+	public int number;
+	public float[] color;
+	public InputDevice controller;
+	public Vector3[] respawn_points;
 }
