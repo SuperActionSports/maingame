672ffccbdb6469511403d360238d7f525fb7b65d
mp3gans <mp3gans@yahoo.com>
2015-10-13 00:00:17 -0400

    Game Control Demo created off of tutorial... need to apply to our wizard

--- SAS/Assets/GameControlDemo.cs
+++ SAS/Assets/GameControlDemo.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 using System.IO;
 using System;
 
-public class GameControlWizard : MonoBehaviour {
-
+public class GameControlDemo : MonoBehaviour {
+	
 	//Singlton design pattern
-	public static GameControlWizard control;
-
+	public static GameControlDemo control;
+	
 	public float health;
 	public float exp;
-
+	
 	// Use this for code that will execute before Start ()
 	void Awake () { 
 		if (control == null) {
@@ -21,36 +21,41 @@ public class GameControlWizard : MonoBehaviour {
 			Destroy(gameObject);
 		} 
 	}
-
+	
+	void onGUI () {
+		GUI.Label (new Rect (10, 10, 100, 30), "Health: " + health);
+		GUI.Label (new Rect (10, 40, 100, 30), "Exp: " + exp);
+	}
+	
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
+	
 	public void Save() {
 		BinaryFormatter bf = new BinaryFormatter ();
 		FileStream file = File.Create (Application.persistentDataPath + "/playerInfo.dat");
-
+		
 		PlayerData data = new PlayerData ();
 		data.health = health;
 		data.exp = exp;
-
+		
 		bf.Serialize (file, data);
 		file.Close ();
 	}
-
+	
 	public void Load() {
 		if (File.Exists (Application.persistentDataPath + "/playerInfo.dat")) {
 			BinaryFormatter bf = new BinaryFormatter();
 			FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
 			PlayerData data = (PlayerData)bf.Deserialize(file);
 			file.Close();
-
+			
 			health = data.health;
 			exp = data.exp;
 		}
@@ -58,7 +63,7 @@ public class GameControlWizard : MonoBehaviour {
 }
 
 [Serializable]
-class GameData {
+class PlayerData {
 	public float health;
 	public float exp;
 }
