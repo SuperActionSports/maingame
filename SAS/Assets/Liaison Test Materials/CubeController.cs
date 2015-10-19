264e3e00551020ba05bb9f857e113320758fcff3
mp3gans <mp3gans@yahoo.com>
2015-10-19 19:20:18 -0400

    LTM - wizard concept test

---
+++ SAS/Assets/Liaison Test Materials/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	public GameObject cubert;
+	public KeyCode forward;
+	public KeyCode backward;
+	public KeyCode left;
+	public KeyCode right;
+	public KeyCode jump;
+	public KeyCode debug_kill;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (forward)) {
+
+		}
+	}
+
+}
