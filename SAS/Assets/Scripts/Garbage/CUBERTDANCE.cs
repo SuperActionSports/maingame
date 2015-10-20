d982e11e81b05a5ebbf0d63b2f773eea95719dd2
mathyoumore <mohr.73@osu.edu>
2015-10-19 20:17:49 -0400

    Austin Garbage
    CUBERT-DANCE-DANCE-REVOLT-U-SHIN

---
+++ SAS/Assets/Scripts/Garbage/CUBERTDANCE.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CUBERTDANCE : MonoBehaviour {
+	private float tOffset;
+	public float height = 2;
+	private float yOffset;
+	// Use this for initialization
+	void Start () {
+		tOffset = Random.Range(0,360);
+		yOffset = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x,Dance()+yOffset,transform.position.z);
+	}
+	
+	float Dance()
+	{
+		return height*Mathf.Sin(Time.time + tOffset);
+	}
+}
