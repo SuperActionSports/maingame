7504685b9ad08ef05b00c5f748af6fee6e382124
tylorlilley <lilley.22@buckeyemail.osu.edu>
2015-10-25 17:14:26 -0400

    More Recent Version of Splatter Creation
    See last commit for ToDos

--- SAS/Assets/Scripts/FX/Paint Splatter/PaintSplatter.cs
+++ SAS/Assets/Scripts/FX/Paint Splatter/PaintSplatter.cs
@@ -15,9 +15,25 @@ public class PaintSplatter : MonoBehaviour {
 		RaycastHit hit;
 		Physics.Raycast (position, direction, out hit);
 
-		var splatRotation = Quaternion.FromToRotation (Vector3.up, hit.normal);
-		GameObject splat = Instantiate (splatter, hit.point, splatRotation) as GameObject;
+		GameObject target = hit.collider.gameObject;
+		GameObject splat = null;
 
-		splat.GetComponent<SpriteRenderer>().color = c;
+		if (target.CompareTag("Stage")) {
+			var splatRotation = Quaternion.FromToRotation (Vector3.forward, hit.normal);
+			splat = Instantiate (splatter, hit.point, splatRotation) as GameObject;
+			splat.transform.localEulerAngles = new Vector3(90, 0, 0);
+		}
+		else if (target.CompareTag("Wall")) {
+			var splatRotation = Quaternion.FromToRotation (Vector3.forward, hit.normal);
+			splat = Instantiate (splatter, hit.point, splatRotation) as GameObject;
+			splat.transform.localEulerAngles = new Vector3(0, 0, 0);
+		}
+		if (splat != null) {
+			splat.GetComponent<SpriteRenderer>().color = c;
+			splat.transform.localScale = new Vector3 (Random.Range (0.1f, 2f), Random.Range (0.1f, 2f), Random.Range (0.1f, 2f));	
+			splat.transform.localEulerAngles = new Vector3(splat.transform.localEulerAngles.x,
+			                                               splat.transform.localEulerAngles.y, 
+			                                               Random.Range (0f, 360f));
+		}
 	}
 }
