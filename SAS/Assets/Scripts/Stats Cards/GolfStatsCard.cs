7629ec7e7f88683ffe272a2f5271fc98969c34d1
mp3gans <mp3gans@yahoo.com>
2015-10-26 20:48:18 -0400

    Golf Stats Card

---
+++ SAS/Assets/Scripts/Stats Cards/GolfStatsCard.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GolfStatsCard : StatsCard {
+	
+	public int puttsAttempted;
+	public int puttsMade;
+	//public int longestPutt;
+	public float accuracy;
+
+	// Use this for initialization
+	void Start () {
+		puttsAttempted = 0;
+		puttsMade = 0;
+		//longestPutt = 0;
+	}
+	
+	public void AddAttemptedPutt()	{
+		puttsAttempted++;
+		CalculateAccuracy ();
+	}
+
+	public void resetAttemptedPutts()	{
+		puttsAttempted = 0;
+	}
+
+	public void AddMadePutt()	{
+		puttsMade++;
+		CalculateAccuracy ();
+	}
+
+	public void ResetMadePutts()	{
+		puttsMade = 0;
+	}
+
+	public float CalculateAccuracy()	{
+		if (puttsAttempted != 0) {
+			accuracy = puttsMade / puttsAttempted;
+		}
+		return accuracy;
+	}
+
+	//Calculate Longest Putt
+	
+}
