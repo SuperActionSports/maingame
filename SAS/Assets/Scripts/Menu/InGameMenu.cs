9a6d694d4119989919a67d3662b4ab257f9f8623
AUSTIN <grosel.9@osu.edu>
2015-10-30 21:22:34 -0400

    Revert "Pause Menu Creation"
    This reverts commit d92eeb7b70f28af9ba594fca356c9130666e4f1c.

--- SAS/Assets/Scripts/Menu/InGameMenu.cs
+++ SAS/Assets/Scripts/Menu/InGameMenu.cs
@@ -1,42 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
-using InControl;
 
 public class InGameMenu : MonoBehaviour {
 
 	public KeyCode escape;
 	public GameObject menu;
-
-	public InputDevice device {get; set;}
-
 	// Use this for initialization
 	void Start () {
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		var inputDevice = InputManager.ActiveDevice;
-		
-		if (StartButtonWasPressedOnDevice( inputDevice ))
+		if (Input.GetKey(escape))
 		{
-			Debug.Log ("Start was pressed pressed");
 			Time.timeScale = 0.0f;
 			menu.SetActive(true);
 		}
 	}
-
-	bool StartButtonWasPressedOnDevice( InputDevice inputDevice )
-	{
-		return inputDevice.Command.WasPressed;
-	}
-
 
 	public void quitMiniGame() {
 		Application.LoadLevel ("AdvancedMainMenu");
 	}
 
 	public void resumeMiniGame() {
-
 		menu.SetActive(false);
 		Time.timeScale = 1.0f;
 	}
