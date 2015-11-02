8ecc1ff96c9c8957534a2f46ee9815fd847b902e
mathyoumore <mohr.73@osu.edu>
2015-11-01 22:35:36 -0500

    Austin-Timebox5 - Master Garbage

---
+++ SAS/Assets/Scripts/Player/GolfClubScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GolfClubScript : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			GolfPlayerController victim = other.GetComponent<GolfPlayerController>();
+			if (victim.alive)
+			{
+				victim.Kill(transform.right*-200f );
+			}
+		}
+	}
+}
