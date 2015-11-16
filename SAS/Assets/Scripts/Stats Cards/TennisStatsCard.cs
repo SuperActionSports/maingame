5c100325bd36f4263875048da6de2a2c88ca1fa3
mp3gans <mp3gans@yahoo.com>
2015-11-16 15:14:48 -0500

    All Scoring is complete except for tennis. waiting on revamp to implement

--- SAS/Assets/Scripts/Stats Cards/TennisStatsCard.cs
+++ SAS/Assets/Scripts/Stats Cards/TennisStatsCard.cs
@@ -7,7 +7,12 @@ public class TennisStatsCard : StatsCard {
 	public int Swings;
 	public int Contact; //Swings that hit the ball not other players
 	public float Accuracy;
+	private int random;
 	
+	public void Start() {
+		random = (int)(Random.Range (0.000f, 4.999f));
+	}
+
 	public void HardResetStats() {
 		ResetSwings ();
 		ResetContact();
@@ -57,4 +62,44 @@ public class TennisStatsCard : StatsCard {
 	*/
 	
 	/*--------------------END TENNIS--------------------*/
+	public float TotalScore() {
+		float Pw = 0.5f;
+		float KDRw = 0.32f;
+		float KSw = 0.1f;
+		float ACCw = 0.05f;
+		float RANw = 0.03f;
+		
+		
+		float RAN;
+		switch (random) {
+		case 0:
+			RAN = jumps;
+			break;
+		case 1:
+			RAN = attemptedAttacks;
+			break;
+		case 2: 
+			RAN = attackSuccessRate;
+			break;
+		case 3:
+			RAN = longestTimeAlive;
+			break;
+		case 4:
+			RAN = shortestTimeAlive;
+			break;
+		default:
+			RAN = 0;
+			break;
+		}
+		
+		float tsP = Contact * Pw;
+		float tsKDR = kDR * KDRw;
+		float tsKS = longestKillStreak * KSw;
+		float tsACC = Accuracy * ACCw;
+		float tsRAN = RAN * RANw;
+		
+		float ts = tsP + tsKDR + tsKS + tsACC + tsRAN;
+		
+		return ts;
+	}
 }
