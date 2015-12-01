f22b8ccd1c4794c1820ac759e69ebb405febf779
mathyoumore <mohr.73@osu.edu>
2015-11-30 19:56:24 -0500

    Parameterized end game GUI

--- SAS/Assets/Scripts/Game GUI/EndgameGUIStatGenerator.cs
+++ SAS/Assets/Scripts/Game GUI/EndgameGUIStatGenerator.cs
@@ -11,51 +11,53 @@ public class EndgameGUIStatGenerator : MonoBehaviour {
 	//private int playerCount;
 	private Player[] players;
 	public GameObject masthead;
+	public bool testingMode = false;
 	//player stat cards
 	
 	void Start()
 	{
-		/*
-		debugPlayers = new Player[4];
-		
-		debugPlayers[0] = new Player();
-		debugPlayers[0].statCard = new StatsCard();
-		debugPlayers[0].color = Color.cyan;
-		debugPlayers[0].statCard.rank = 0;
-		debugPlayers[0].statCard.kills = 15;
-		debugPlayers[0].statCard.deaths = 4;
-		debugPlayers[0].statCard.longestKillStreak = 3;
-		debugPlayers[0].statCard.longestTimeAlive = 74.386543682f;
-		
-		debugPlayers[1] = new Player();
-		debugPlayers[1].statCard = new StatsCard();
-		debugPlayers[1].color = Color.magenta;
-		debugPlayers[1].statCard.rank = 1;
-		debugPlayers[1].statCard.kills = 10;
-		debugPlayers[1].statCard.deaths = 8;
-		debugPlayers[1].statCard.longestKillStreak = 1;
-		debugPlayers[1].statCard.longestTimeAlive = 60.265436985219f;
-		
-		debugPlayers[2] = new Player();
-		debugPlayers[2].statCard = new StatsCard();
-		debugPlayers[2].color = Color.yellow;
-		debugPlayers[2].statCard.rank = 2;
-		debugPlayers[2].statCard.kills = 8;
-		debugPlayers[2].statCard.deaths = 12;
-		debugPlayers[2].statCard.longestKillStreak = 1;
-		debugPlayers[2].statCard.longestTimeAlive = 48.235365879f;
-		
-		debugPlayers[3] = new Player();
-		debugPlayers[3].statCard = new StatsCard();
-		debugPlayers[3].color = Color.red;
-		debugPlayers[3].statCard.rank = 3;
-		debugPlayers[3].statCard.kills = 3;
-		debugPlayers[3].statCard.deaths = 35;
-		debugPlayers[3].statCard.longestKillStreak = 1;
-		debugPlayers[3].statCard.longestTimeAlive = 31.574104829f;
-		
-		SetPlayers = debugPlayers;
-		*/
+		if (testingMode)
+		{
+			debugPlayers = new Player[4];
+			
+			debugPlayers[0] = new Player();
+			debugPlayers[0].statCard = new StatsCard();
+			debugPlayers[0].color = Color.cyan;
+			debugPlayers[0].statCard.rank = 0;
+			debugPlayers[0].statCard.kills = 15;
+			debugPlayers[0].statCard.deaths = 4;
+			debugPlayers[0].statCard.longestKillStreak = 3;
+			debugPlayers[0].statCard.longestTimeAlive = 74.386543682f;
+			
+			debugPlayers[1] = new Player();
+			debugPlayers[1].statCard = new StatsCard();
+			debugPlayers[1].color = Color.magenta;
+			debugPlayers[1].statCard.rank = 1;
+			debugPlayers[1].statCard.kills = 10;
+			debugPlayers[1].statCard.deaths = 8;
+			debugPlayers[1].statCard.longestKillStreak = 1;
+			debugPlayers[1].statCard.longestTimeAlive = 60.265436985219f;
+			
+			debugPlayers[2] = new Player();
+			debugPlayers[2].statCard = new StatsCard();
+			debugPlayers[2].color = Color.yellow;
+			debugPlayers[2].statCard.rank = 2;
+			debugPlayers[2].statCard.kills = 8;
+			debugPlayers[2].statCard.deaths = 12;
+			debugPlayers[2].statCard.longestKillStreak = 1;
+			debugPlayers[2].statCard.longestTimeAlive = 48.235365879f;
+			
+			debugPlayers[3] = new Player();
+			debugPlayers[3].statCard = new StatsCard();
+			debugPlayers[3].color = Color.red;
+			debugPlayers[3].statCard.rank = 3;
+			debugPlayers[3].statCard.kills = 3;
+			debugPlayers[3].statCard.deaths = 35;
+			debugPlayers[3].statCard.longestKillStreak = 1;
+			debugPlayers[3].statCard.longestTimeAlive = 31.574104829f;
+			
+			SetPlayers = debugPlayers;
+		}
 		
 	}
 	
